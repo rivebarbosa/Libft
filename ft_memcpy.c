5c3ef864ba22d4ce71c76ab832ed512748fac1bd
rivebarbosa <roberto.rivebarbosa@gmail.com>
2022-09-11 17:55:16 -0300

    fourcommit

---
+++ ft_memcpy.c
@@ -0,0 +1,18 @@
+#include <libft.h>
+
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    unsigned char   *s;
+    unsigned char   *d;
+
+    if (src == NULL && dest == NULL)
+        return (dest);
+    s = (unsigned char *)src;
+    d = (unsigned char *)dest;
+    while (n--)
+    {
+        *d++ = *s++;
+        return(dest);
+    }
+    
+}
