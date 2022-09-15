27af13d3368b9abe3ab69607823a3d45e6db6b05
Roberto Rivelino Barbosa <rrivelin@c1r7p3.42sp.org.br>
2022-09-14 21:28:55 -0300

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,45 +6,76 @@
 /*   By: rrivelin <rrivelin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/13 21:25:46 by rrivelin          #+#    #+#             */
-/*   Updated: 2022/09/14 20:57:27 by rrivelin         ###   ########.fr       */
+/*   Updated: 2022/09/14 21:28:20 by rrivelin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
+#include <stdlib.h>
+
+// void	*ft_memmove(void *destination, const void *source, size_t n)
+// {
+// 	unsigned char	*dest;
+// 	unsigned char	*src;
+// 	size_t			i;
+
+// 	dest = (unsigned char *)destination;
+// 	src = (unsigned char *)source;
+// 	if (dest == src)
+// 		return (destination);
+// 	if (dest > src)
+// 	{
+// 		i = 0;
+// 		while (++i <= n)
+// 			dest[n - i] = src[n - i];
+// 	}
+// 	else
+// 	{
+// 		i = 0;
+// 		while (i < n)
+// 		{
+// 			dest[i] = src[i];
+// 			i++;
+// 		}
+// 	}
+// 	return (destination);
+// }
+
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char	*source;
+	unsigned char	*dest;
+	size_t			i;
+
+	source = (unsigned char *)src;
+	dest = (unsigned char *)dst;
+	if (dest == source)
+	{
+		return (dst);
+	}
+	if (dest > source)
+	{
+		i = 0;
+		while (++i <= n)
+			dest[n - i] = source[n - i];
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			dest[i] = source[i];
+			i++;
+		}
+	}
+	return (dest);
+}
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void	*ft_memmove(void *dst, const void *src, size_t n)
-{
-	unsigned char	*s;
-	unsigned char	*d;
-	unsigned char	*storage;
-	int				i;
-
-	s = (unsigned char *)src;
-	d = (unsigned char *)dst;
-	i = 0;
-	while (i < n)
-	{
-		storage[i] = d[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		s[i] = d[i];
-		i++;
-	}
-	i = 0;
-	while (i < n)
-	{
-		s[i] = storage[i];
-		i++;
-	}
-}
 
 
 
